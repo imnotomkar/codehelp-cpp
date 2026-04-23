8d66d8767632e466f9d0d5057d58d5389ed64b11
Omkar Tuplondhe <omkartuplondhe721@gmail.com>
2026-04-23 20:15:00 +0530

    Add files via upload

---
+++ ternary.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int  main(){
+
+    //SCORPIO LENE KELIYE BUDGET CODE
+    int budget;
+
+    cout<<"BUDGET BATA CHL BHAI: "<<endl;
+    cin>>budget;
+
+    (budget>2000000) ? cout<<"le sakta bhai" : cout<<"jana bhai majak kara kya?";
+
+    return 0;
+}
