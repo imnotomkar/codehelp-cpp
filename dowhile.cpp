375ce50bc0938730e94f4d8a0f7a21413753b158
Omkar Tuplondhe <omkartuplondhe721@gmail.com>
2026-04-23 22:39:47 +0530

    Add files via upload

---
+++ dowhile.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char i='Z';
+
+    do{
+        
+        cout<<i<<endl;
+        i=i-1;
+    }while(i>='A');
+    
+return 0; 
+}
---
+++ forcontinue.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=100;i>=1;i=i-1){
+        
+        if(i==69){
+            continue;
+        }
+        cout<<i<<endl;
+    }
+}
