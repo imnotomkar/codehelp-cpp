d46994b840ce6629a7709ca2e26166461026fdf0
Omkar Tuplondhe <omkartuplondhe721@gmail.com>
2026-04-23 23:09:30 +0530

    Add files via upload

---
+++ nestedloop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    for(int i=1;i<=3;i=i+1){
+        for(int j=1;j<=3;j=j+1){
+            cout<<i<<j<<" ";
+        }
+
+    }
+    return 0;
+}
