fad88159a76e4dc67e9d7442e4810c533ab675fe
Omkar Tuplondhe <omkartuplondhe721@gmail.com>
2026-04-23 22:38:44 +0530

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    
+    //le bhai bandi ko sorry bolde 100 baar maan jaygi
+    for(int i=1;i<=100;i=i+1){
+        cout<<i<<"."<<"sorry"<<endl;
+    }
+
+
+    return 0;
+}
