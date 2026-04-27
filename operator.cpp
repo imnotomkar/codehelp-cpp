81298c43c36b7edafe5ffb650443585e778c6f95
Omkar Tuplondhe <omkartuplondhe721@gmail.com>
2026-04-28 02:36:50 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //URANARY OPERATOR
+    // int a=5;
+
+   //  cout<<a<<endl;
+   //  cout<<++a<<endl;
+   //  cout<<--a<<endl;
+   //  cout<<a++<<endl;
+   //  cout<<a--<<endl;
+   //  cout<<a<<endl;
+    
+    //secondary operator
+    //arthematic operator
+
+   //  int a=5,b=10;
+    
+   //  cout<<(a+b)<<endl;
+   //  cout<<(a-b)<<endl;
+   //  cout<<(a*b)<<endl;
+   //  cout<<(a/b)<<endl;
+   //  cout<<(a%b)<<endl;
+
+    //relational operator
+    
+  //  int a=5,b=4;
+  //  cout<<(a==b)<<endl;
+  //  cout<<(a>b)<<endl;
+  //  cout<<(a<b)<<endl;
+  //  cout<<(a=!b)<<endl;
+    
+    //logical operator
+    
+  //  bool con1=true;
+  //  bool con2=false;
+  //  bool con3=true;
+
+  //  cout<<(con1&&con2&&con3)<<endl;
+  //  cout<<(con1||con2||con3)<<endl;
+  //   cout<<(!con1)<<endl;
+
+    //bitwise operator
+
+  //  cout<<(5&4)<<endl;
+  //  cout<<(5|4)<<endl;
+  //  cout<<(5^4)<<endl;
+  //  cout<<(~5)<<endl;
+
+    //assinging operator
+
+  //  int a=5;
+
+  //  cout<<(a+=5)<<endl;
+  //  cout<<(a-=5)<<endl;
+  //  cout<<(a*=5)<<endl;
+  //  cout<<(a/=5)<<endl;
+    
+    return 0;
+}
